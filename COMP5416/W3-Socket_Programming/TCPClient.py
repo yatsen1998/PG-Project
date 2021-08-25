8fcfe42de34ee7c20a02b6d8cb9a9b4d0b3f0a97
Evan Xu <tzxuyiwei100@126.com>
2021-08-25 18:05:47 +0800

    w3: typo test

---
+++ COMP5416/W3-Socket_Programming/TCPClient.py
@@ -0,0 +1,11 @@
+from socket import *
+serverName = ’servername’
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+sentence = input(‘Input lowercase sentence:’)
+clientSocket.send(sentence.encode('utf-8’))
+								## Do not specify serverName,serverPort
+modifiedSentence = clientSocket.recv(1024)
+print ('From Server:', modifiedSentence.decode('utf-8’))
+clientSocket.close()
---
+++ COMP5416/W3-Socket_Programming/TCPServer.py
@@ -0,0 +1,14 @@
+from socket import *
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind((‘’, serverPort))
+serverSocket.listen(1)
+print(‘The server is ready to receive’)
+while 1:
+    connectionSocket, addr = serverSocket.accept()
+
+    sentence = connectionSocket.recv(1024)
+    capitalizedSentence = sentence.decode('utf-8').upper().encode('utf-8’)
+    connectionSocket.send(capitalizedSentence)
+    connectionSocket.close()
