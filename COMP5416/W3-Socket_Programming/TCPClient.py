025210c301a8300d708b4b440402749f0bebe10d
Evan Xu <tzxuyiwei100@126.com>
2021-08-25 19:05:10 -0700

    w3: socket programming: change host name

--- COMP5416/W3-Socket_Programming/TCPClient.py
+++ COMP5416/W3-Socket_Programming/TCPClient.py
@@ -1,6 +1,6 @@
 from socket import *
 
-serverName = 'servername'
+serverName = '127.0.0.1'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
--- COMP5416/W3-Socket_Programming/UDPClient.py
+++ COMP5416/W3-Socket_Programming/UDPClient.py
@@ -1,7 +1,7 @@
 from socket import *
-serverName = 'hostname'
+serverName = '127.0.0.1'
 serverPort = 12000
-clientSocket = socket(socket.AF_INET, socket.SOCK_DGRAM)
+clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input('Input lowercase sentence:')
 message = message.encode('utf-8')
 clientSocket.sendto(message, (serverName, serverPort))
