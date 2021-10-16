1826b0750b886cbbdd4895099ce4a52179880873
Evan Xu <tzxuyiwei100@126.com>
2021-10-16 15:10:44 +0800

    comp5416: W8: pics clean

---
+++ COMP5416/W8-Sending:Receiving_Images/server/server.py
@@ -0,0 +1,28 @@
+from socket import *
+import uuid
+
+serverPort = 12010
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(5)
+
+print("Server Started...")
+print("Waiting for other connections")
+
+conn_socket, addr = serverSocket.accept()
+print("Connection established, receiving data...")
+
+f = open(str(uuid.uuid4()), 'wb')
+l = conn_socket.recv(1024)
+
+while True:
+    f.write(l)
+    l = conn_socket.recv(1024)
+    if (len(l)==0):
+        break
+
+f.close()
+
+print("All data received, Connection lost")
+
+conn_socket.close()`
