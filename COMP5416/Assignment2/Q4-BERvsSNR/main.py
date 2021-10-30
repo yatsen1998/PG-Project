0a51e361d7981bd220962d672f072a6a4f21d663
Evan Xu <tzxuyiwei100@126.com>
2021-10-30 19:21:53 +0800

    comp5416: assignment2: q4 4QAM added

--- COMP5416/Assignment2/Q4-BERvsSNR/main.py
+++ COMP5416/Assignment2/Q4-BERvsSNR/main.py
@@ -3,17 +3,28 @@ import math
 
 
 def BPSK_func(snr):
-    sigma = math.sqrt(1/math.pow(10, snr / 10))
-    return (math.erfc((1/sigma)/math.sqrt(2)))/2
+    sigma = math.sqrt(1 / math.pow(10, snr / 10))
+    return (math.erfc((1 / sigma) / math.sqrt(2))) / 2
+
+
+def FourQAM_func(snr):
+    sigma = math.sqrt(1 / (2 * (math.pow(10, snr / 10))))
+    q = (math.erfc((1 / sigma) / math.sqrt(2))) / 2
+    return 2 * q - q * q
 
 
 dB = 0
 x_list = []
 bpsk_list = []
+fourQAM_list = []
 while dB <= 25:
     x_list.append(dB)
     bpsk_list.append(BPSK_func(dB))
-    print("x: %d, y: %s" % (dB, format(BPSK_func(dB), '.8E')))
+    fourQAM_list.append(FourQAM_func(dB))
+
+    print("BPSK x: %d, y: %s" % (dB, format(BPSK_func(dB), '.8E')))
+    print("4QAM x: %d, y: %s" % (dB, format(FourQAM_func(dB), '.8E')))
+
     dB += 5
 
 fig, ax = plt.subplots()
@@ -24,5 +35,7 @@ plt.ylabel('BER')
 plt.yscale('log')
 
 plt.plot(x_list, bpsk_list, color='r', label='BPSK')
+plt.plot(x_list, fourQAM_list, color='g', label='4QAM')
 
+plt.legend()
 plt.show()
