b7ea64c02094d0f599d04e5b801a206d2f97891f
Evan Xu <tzxuyiwei100@126.com>
2021-11-05 23:39:28 +0800

    comp5416: assignment2: q2: modify codes

--- COMP5416/Assignment2/Q2-Token_Bucket/TokenBucket.py
+++ COMP5416/Assignment2/Q2-Token_Bucket/TokenBucket.py
@@ -1,56 +1,76 @@
 import numpy as np
+import math
 
-Capacity = 4
-
-class TokenBucket:
-    def __init__(self, arrival_rate, capacity)
-
-T = 20
-arate = 3
-drate = 4
-
+T = 10000
+arrival_rate = 1
+departure_rate = 1.25
+r = 1 / departure_rate
 t = 0
+
+# packet arrival in Poisson Distribution
 arrival = []
 while t < T:
-    t = t + np.random.exponential(1.0 / arate)
+    t = t + np.random.exponential(1.0 / arrival_rate)
     arrival.append(t)
 
 t = 0
+told = 0
 N = 0
+flag = False
+Tokens = 4
 departure = []
 recording = []
 
-while (t < T and len(arrival) > 0):
+while t < T and len(arrival) > 0:
 
-    if len(departure) == 0:
+    if Tokens < 4:
+        Tokens += 1
+        if Tokens > 4:
+            Tokens = 4
 
-        told = t
-        t = arrival[0]
-        del arrival[0]
-        de = t + np.random.exponential(1.0 / drate)
-        departure.append(de)
-        recording.append([told, t, N])
-        N = N + 1
-    else:
+    while Tokens > 0 and len(arrival) > 0:
 
-        if departure[0] < arrival[0]:
-            told = t
-            t = departure[0]
-            del departure[0]
-            recording.append([told, t, N])
+        if len(departure) == 0:
 
-            N = N - 1
-            if N >= 1:
-                de = t + np.random.exponential(1.0 / drate)
-                departure.append(de)
-                departure.sort()
-
-        else:
             told = t
             t = arrival[0]
             del arrival[0]
-            recording.append([told, t, N])
+            de = t + np.random.exponential(1.0 / departure_rate)
+            departure.append(de)
+            Tokens = Tokens - 1
+            recording.append([told, t, N, - Tokens])
             N = N + 1
+
+        else:
+
+            if departure[0] < arrival[0]:
+                if flag:
+                    told = lastt
+                else:
+                    told = t
+                t = departure[0]
+                del departure[0]
+                recording.append([told, t, N, - Tokens])
+
+                N = N - 1
+                if N >= 1:
+                    de = t + np.random.exponential(1.0 / departure_rate)
+                    departure.append(de)
+                    departure.sort()
+
+            else:
+                if flag:
+                    told = lastt
+                else:
+                    told = t
+                t = arrival[0]
+                del arrival[0]
+                Tokens = Tokens - 1
+                recording.append([told, t, N, - Tokens])
+                N = N + 1
+    flag = True
+    lastt = t
+    t = t + 1 / r
 
 duration = []
 x1 = 0.0
@@ -63,6 +83,8 @@ Mnumber = [x[2] for x in recording]
 M = max(Mnumber)
 distribution = [0 for i in range(M + 1)]
 
+one_packet_prob = 0
+
 for i in range(0, len(recording)):
     state = recording[i][2]
     distribution[state] = distribution[state] + recording[i][1] - recording[i][0]
@@ -73,10 +95,5 @@ sumtime = sum(distribution)
 pdf = [x / sumtime for x in distribution]
 print(pdf)
 
-for i in range(0, len(recording)):
-    print(recording[i])
-
-
-
-
-
+# for i in range(0, len(recording)):
+#     print(recording[i])
