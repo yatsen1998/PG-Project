329432c34b7c9275ccc15b7a12801e0df11e2207
Evan Xu <tzxuyiwei100@126.com>
2022-04-01 15:48:38 +0800

    COMP5426: Assignment1: Add a makefile

--- COMP5426/Assignment1/Baseline.c
+++ COMP5426/Assignment1/Baseline.c
@@ -1,63 +1,45 @@
-/*
-    Assignment1 for COMP5426
-    Baseline with Pthread
-*/
-
+// Assignment1 for COMP5426
 #include <stdio.h>
 #include <stdlib.h>
-#include<stdbool.h>
+#include <stdbool.h>
 #include <sys/time.h>
 #include <pthread.h>
 #include <math.h>
-
-#define MIN(a, b) a <= b ? a : b;
 
 int N;
 int M;
 int T;
 int workload = 0;
 double** sequences;
-double* V;
-int* R;
+double* result;
 
 struct timeval start_time, end_time;
-struct coordinate
-{
-    int i;
-    int j;
-};
-
-void getCoordinate(int k, struct coordinate* co)
-{
-    for (int i = 0; i < N; i++) {
-        for (int j = i; j < M; j++) {
-            if (k == R[i] + j - i) {
-                co->i = i;
-                co->j = j;
-            }
-        }
-    }
-}
 
 void* pthread_baseline(void* threadId)
 {
+    int count = 0;
+    double sum = 0;
     int* id;
 
     id = (int*) threadId;
 
-    int start = workload * (*id);
-    int end = MIN(workload * ((*id) + 1), N * (N + 1) / 2);
-    struct coordinate* co;
-    co = (struct coordinate*) malloc (sizeof(struct coordinate));
+    for (int i = 0; i < N; i++) {
+        for (int j = i; j < N; j++) {
+            if(count >= workload * (*id) && count < workload * ((*id) + 1)) {
+                //printf("%d count: %d\n", (*id), count);
+                sum = 0;
+                for (int z = 0; z < M; z++) {
+                    sum += sequences[i][z] * sequences[j][z];
+                }
+                result[count] = sum;
+                count++;
+                //printf("%d, result[%d]: %2lf\n", (*id), count, sum);
+            } else {
+                count++;
+                continue;
+            }
 
-    printf("id:%d start:%d, end:%d\n", (*id), start, end);
-    for (int k = start; k < end; ++k) {
-        getCoordinate(k, co);
-
-        for (int z = 0; z < M; z++) {
-            V[k] +=  sequences[co->i][z] * sequences[co->j][z];
         }
-        //printf("k:%d i:%d j:%d V[k]:%2lf\n", k, co->i, co->j, V[k]);
     }
 
     return NULL;
@@ -73,8 +55,8 @@ bool check_result()
             for (int z = 0; z < M; z++) {
                 sum += sequences[i][z] * sequences[j][z];
             }
-            if (V[count] != sum) {
-                printf("count:%d V[count]:%lf sum:%lf\n", count, V[count], sum);
+            if (result[count] != sum) {
+                printf("count:%d V[count]:%lf sum:%lf\n", count, result[count], sum);
                 return false;
             }
             count++;
@@ -92,14 +74,7 @@ int main(int argc, char* argv[])
 
     /* Allocate result array. */
     int result_size = N * (N + 1) / 2;
-    V = (double*) malloc(result_size * sizeof(double));
-
-    /* Allocate pivot array */
-    R = (int*) malloc(N * sizeof(int));
-    R[0] = 0;
-    for (int i = 1; i < N; i++) {
-        R[i] = R[i - 1] + N - i + 1;
-    }
+    result = (double*) malloc(result_size * sizeof(double));
 
     /* Generate Sequence matrix randomly. */
     double* sequence;
@@ -115,9 +90,8 @@ int main(int argc, char* argv[])
         }
     }
     
-    /* Baseline pthread implementation */
-    
-    int rc = 0;
+    /* Generate threads */
+    int ret = 0;
     workload = ceil((double)result_size / T);
     int tids[num_threads];
     pthread_t threads[num_threads];
@@ -128,11 +102,12 @@ int main(int argc, char* argv[])
 
     for (int i = 0; i < num_threads; ++i) {
         tids[i] = i;
-        rc = pthread_create(&threads[i], NULL, pthread_baseline, (void*) &tids[i]);
-        if (rc) {
+        ret = pthread_create(&threads[i], NULL, pthread_baseline, (void*) &tids[i]);
+        if (ret) {
             printf("Pthread Create Failed.\n");
             exit(-1);
         }
+        // As the threads are calcultating at almost the same time, we need to lock the sum to make sure there are no conflict.
     }
 
     for (int i = 0; i < num_threads; i++) {
@@ -144,11 +119,13 @@ int main(int argc, char* argv[])
     long microseconds = end_time.tv_usec - start_time.tv_usec;
     double elapsed = seconds + 1e-6 * microseconds;
 
-    printf("Baseline Pthread method took %2f seconds to complete.\n\n", elapsed);
+    printf("It took %f seconds to complete.\n\n", elapsed);
 
     // for (int i = 0; i < result_size; i++) {
     //     printf("%2lf ", result[i]);
     // }
+
+    /* Deal with remain sequences. */
 
     bool check = check_result();
 
@@ -159,7 +136,7 @@ int main(int argc, char* argv[])
 
     free(sequence);
     free(sequences);
-    free(V);
+    free(result);
 
     return 0;
 }
