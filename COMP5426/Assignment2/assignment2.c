f510677776765512de86b579ad7ccdf68a978013
Evan Xu <tzxuyiwei100@126.com>
2022-05-04 20:30:27 +0800

    COMP5426: Assignment2: Fix wrong computation

--- COMP5426/Assignment2/assignment2.c
+++ COMP5426/Assignment2/assignment2.c
@@ -24,6 +24,8 @@ double* V;
 
 struct timeval start_time, end_time;
 
+
+void print_matrix(double* mat, int m, int n);
 void matMultiplyWithSingleThread(double* A, double* B, int m, int n, int x, int y);
 void computeMatrix(int i, int j);
 void* pthread_efficient(void* threadId);
@@ -34,7 +36,7 @@ int main(int argc, char* argv[])
     N = atoi(argv[1]);
     M = atoi(argv[2]);
     T = atoi(argv[3]);
-    B = atoi(argv[4]);
+
     int num_threads = T;
 
     int myid, numprocs;
@@ -43,6 +45,8 @@ int main(int argc, char* argv[])
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+    B = N / numprocs;
 
     double* sequence;
     int blockN = N / B; // To simplify the calculation, demanded by requirement
@@ -54,6 +58,7 @@ int main(int argc, char* argv[])
 
     /* Allocate result array. */
     V = (double*) malloc(result_size * sizeof(double));
+    double* allV = NULL;
 
     /* Allocate pivot array */
     R = (int*) malloc(N * sizeof(int));
@@ -62,13 +67,15 @@ int main(int argc, char* argv[])
      *  OwnedBlock denotes the unchanged matrix of each process.
      *  ExchangedBlock denotes the exchanged matrix after each step. 
      */
-    double* OwnedBlock = (double*) malloc(blockN * M * sizeof (double)); 
-    double* ExchangedBlock = (double*) malloc(M * blockN * sizeof (double)); 
+    double* OwnedBlock = (double*) malloc(B * M * sizeof (double)); 
+    double* ExchangedBlock = (double*) malloc(M * B * sizeof (double)); 
 
     if (myid == 0) {
         for (int i = 0; i < result_size; i++) {
             V[i] = 0;
         }
+
+        allV = (double*)malloc(blockN * result_size * sizeof(double));
 
         R[0] = 0;
         for (int i = 1; i < N; i++) {
@@ -81,9 +88,9 @@ int main(int argc, char* argv[])
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < M; ++j) {
                 sequences[i * M + j] = (double) rand() / RAND_MAX;
-                printf("%lf ", sequences[i * M + j]);
+                // printf("%lf ", sequences[i * M + j]);
             }
-            printf("\n");
+            // printf("\n");
         }
 
     }
@@ -92,25 +99,44 @@ int main(int argc, char* argv[])
     MPI_Bcast(R, N, MPI_INT, 0, MPI_COMM_WORLD);
 
     MPI_Scatter(sequences, B * M, MPI_DOUBLE, OwnedBlock, B * M, MPI_DOUBLE, 0, MPI_COMM_WORLD); // Scatter the blocked matrix to each process
-
-    ExchangedBlock = OwnedBlock;
+    MPI_Scatter(sequences, B * M, MPI_DOUBLE, ExchangedBlock, B * M, MPI_DOUBLE, 0, MPI_COMM_WORLD); // Scatter the blocked matrix to each process
 
     int leftRank = (myid + numprocs - 1) % numprocs;
     int rightRank = (myid + 1) % numprocs;
-
-    printf("\n");
 
     for (int i = 0; i < steps; i++) {
         matMultiplyWithSingleThread(OwnedBlock, ExchangedBlock, B, M, myid, (myid + i) % numprocs);
         //ComputeMatrixWithThreads(ExchangedBlock);
 
-        MPI_Sendrecv_replace((void*)ExchangedBlock, M * B, MPI_FLOAT, leftRank, 0, rightRank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Sendrecv_replace((void*)ExchangedBlock, M * B * 2, MPI_FLOAT, leftRank, 0, rightRank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
 
+    // if (myid == 2) {
+    //     for (int i = 0; i < result_size; i++) {
+    //         printf("P:%d V[%d]:%lf\n", myid, i, V[i]);
+    //     }
+    // }
+
+    MPI_Gather(V, result_size, MPI_DOUBLE, allV, result_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
     if (myid == 0) {
-        for (int i = 0; i < result_size; i++) {
-            printf("V[%d]:%lf\n", i, V[i]);
+        for (int i = 0; i < blockN * result_size; i++) {
+            if (allV[i] != 0) {
+                V[i % result_size] = allV[i];
+            }
+            // printf("V[%d]:%lf\n", i % result_size, allV[i]);
         }
+
+        // for (int i = 0; i < result_size; i++) {
+        //     printf("V[%d]:%lf\n", i, V[i]);
+        // }
+
+        bool check = check_result();
+
+        if (!check)
+            printf("Calculation is not correct.\n");
+        else 
+            printf("Calculation is correct!\n");
     }
 
     // /* Calculating Matrix */
@@ -133,19 +159,14 @@ int main(int argc, char* argv[])
     //     pthread_join(threads[i], NULL);
     // }
 
-    // // gettimeofday(&end_time, 0);
-    // // seconds = end_time.tv_sec - start_time.tv_sec;
-    // // microseconds = end_time.tv_usec - start_time.tv_usec;
-    // // elapsed = seconds + 1e-6 * microseconds;
+    // gettimeofday(&end_time, 0);
+    // seconds = end_time.tv_sec - start_time.tv_sec;
+    // microseconds = end_time.tv_usec - start_time.tv_usec;
+    // elapsed = seconds + 1e-6 * microseconds;
 
-    // // printf("Efficient Algorithm took %lf seconds to complete.\n\n", elapsed);
+    // printf("Efficient Algorithm took %lf seconds to complete.\n\n", elapsed);
 
-    // // bool check = check_result();
 
-    // // if (!check)
-    // //     printf("Calculation is not correct.\n");
-    // // else 
-    // //     printf("Calculation is correct!\n");
 
     free(sequences);
     free(R);
@@ -156,6 +177,16 @@ int main(int argc, char* argv[])
     return 0;
 }
 
+void print_matrix(double* mat, int m, int n)
+{
+    for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                printf("%lf ", mat[i * n + j]);
+            }
+            printf("\n");
+        }
+}
+
 void matMultiplyWithSingleThread(double* matA, double* matB, int m, int n, int x, int y)
 {
     double* matResult = (double*) malloc(m * m * sizeof(double));
@@ -163,12 +194,12 @@ void matMultiplyWithSingleThread(double* matA, double* matB, int m, int n, int x
         for (int j = 0; j < m; j++) {
             double tmp = 0;
             if (x == y && i > j) {
-                matResult[i * m + j] = tmp;
+                matResult[i * m + j] = 0;
                 continue;
             }
 
             for (int z = 0; z < n; z++) {
-                tmp += matA[i * m + z] * matB[j * n + z];
+                tmp += matA[i * n + z] * matB[j * n + z];
             }
             matResult[i * m + j] = tmp;
         }
@@ -189,8 +220,6 @@ void matMultiplyWithSingleThread(double* matA, double* matB, int m, int n, int x
             V[k] = matResult[i * m + j];
         }
     }
-
-    
 }
 
 // void computeMatrix(int i, int j)
@@ -241,22 +270,30 @@ void matMultiplyWithSingleThread(double* matA, double* matB, int m, int n, int x
 //     return NULL;
 // }
 
-// bool check_result()
-// {
-//     int count = 0;
-//     double sum = 0;
-//     for (int i = 0; i < N; i++) {
-//         for (int j = i; j < N; j++) {
-//             sum = 0;
-//             for (int z = 0; z < M; z++) {
-//                 sum += sequences[i][z] * sequences[j][z];
-//             }
-//             if (fabs(V[count] - sum) > 0.000001) {
-//                 printf("Efficient Computation Error count:%d V[count]:%lf sum:%lf\n", count, V[count], sum);
-//                 return false;
-//             }
-//             count++;
-//         }
-//     }
-//     return true;
-// }
+bool check_result()
+{
+    int count = 0;
+    double sum = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = i; j < N; j++) {
+            sum = 0;
+            for (int z = 0; z < M; z++) {
+                sum += sequences[i * M + z] * sequences[j * M + z];
+            }
+
+            if (fabs(V[count] - sum) > 0.000001) {
+                for (int z = 0; z < M; z++) {
+                    printf("%lf ", sequences[i * M + z]);
+                }
+                printf("\n");
+                for (int z = 0; z < M; z++) {
+                    printf("%lf ", sequences[j * M + z]);
+                }
+                printf("Computation Error count:%d V[count]:%lf sum:%lf\n", count, V[count], sum);
+                return false;
+            }
+            count++;
+        }
+    }
+    return true;
+}
